fb239b45a78a0eecbd2aca7ebf61691e131181a4
Frank Eppink <frank.eppink@live.de>
2021-03-21 12:05:55 +0100

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Evil World!'));
+app.get('/', (req, res) => res.send('Hello Happy World! Greetings from Frankie :-)'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
